90b2a861a58c0db2dc38ae0ab01472fef61b27bd
Jannik Lorenz <dev@janniklorenz.de>
2015-08-04 13:48:38 +0200

    add layout basics and bootstrap

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var app = express()
 var http = require("http")
 var config = require("./config/")
 var fs = require("fs")
-
+var expressLess = require('express-less')
 
 
 
 app.set('view engine', 'jade');
+
+
+app.use("/js/", express.static("./assets/js"))
+app.use("/js/", express.static("./node_modules/jquery/dist"))
+
+app.use("/css/", expressLess(__dirname + "/stylesheets"))
 
 app.get("/", function(req, res){
 	res.render("index.jade")
